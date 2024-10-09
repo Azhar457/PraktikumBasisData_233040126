bafff257a836becde377cf6db207f33c4bd1e10d
moonford <fixrot234@gmail.com>
2024-10-09 18:45:32 +0700

    Alhamdulillah selesai

---
+++ pertemuan1/Latihan1/Soal1.sql
@@ -0,0 +1,8 @@
+USE kelas_d;
+SELECT c.firstname, c.lastname, 
+CASE 
+    WHEN c.titleofcourtesy IN ('Ms.', 'Mrs.') THEN 'Female'
+    WHEN c.titleofcourtesy = 'Mr.' THEN 'Male'
+    ELSE 'Unknown'
+END AS 'Gender'
+FROM HR.Employees c;
